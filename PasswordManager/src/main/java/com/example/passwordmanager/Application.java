65118e8f6178cd7635f03e74a286455f614b54aa
diaaotoomHTU <83277185+diaaotoomHTU@users.noreply.github.com>
2022-12-29 19:03:45 +0300

    Commit

--- PasswordManager/src/main/java/com/example/passwordmanager/Application.java
+++ PasswordManager/src/main/java/com/example/passwordmanager/Application.java
@@ -2,16 +2,24 @@ package com.example.passwordmanager;
 
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class Application extends javafx.application.Application {
+
+    static Stage loadedStage;
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("login.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
+        scene.getStylesheets().add(Application.class.getResource("styles/nord-light.css").toString());
+        loadedStage = stage;
         stage.setTitle("Password Manager");
+        stage.getIcons().add(new Image(Application.class
+                .getResource("images/lock-icon.png").toString()));
         stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
--- PasswordManager/src/main/java/com/example/passwordmanager/Controller.java
+++ PasswordManager/src/main/java/com/example/passwordmanager/Controller.java
@@ -3,13 +3,18 @@ package com.example.passwordmanager;
 import javafx.beans.Observable;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+
+import java.io.IOException;
 
 public class Controller {
     @FXML
@@ -48,4 +53,65 @@ public class Controller {
         newPasswordEntryChildren.add(revealButton);
         passwordBox.getChildren().add(newPasswordEntry);
     }
+
+    @FXML
+    protected void getPasswordManagerScene() throws IOException {
+        Parent pane = FXMLLoader.load(getClass().getResource("password-manager.fxml"));
+        Application.loadedStage.getScene().setRoot(pane);
+        Application.loadedStage.sizeToScene();
+        Application.loadedStage.centerOnScreen();
+    }
+
+    @FXML
+    protected void getNewPasswordScene() throws IOException {
+        Parent pane = FXMLLoader.load(getClass().getResource("new-password.fxml"));
+        Application.loadedStage.getScene().setRoot(pane);
+        Application.loadedStage.sizeToScene();
+        Application.loadedStage.centerOnScreen();
+    }
+
+    @FXML
+    protected void getDeletePasswordScene() throws IOException {
+        Parent pane = FXMLLoader.load(getClass().getResource("delete-password.fxml"));
+        Application.loadedStage.getScene().setRoot(pane);
+        Application.loadedStage.sizeToScene();
+        Application.loadedStage.centerOnScreen();
+    }
+
+    @FXML
+    protected void getUserInfoScene() throws IOException {
+        Parent pane = FXMLLoader.load(getClass().getResource("user-info.fxml"));
+        Application.loadedStage.getScene().setRoot(pane);
+        Application.loadedStage.sizeToScene();
+        Application.loadedStage.centerOnScreen();
+    }
+
+    @FXML
+    protected void getMasterPasswordScene() throws IOException {
+        Parent pane = FXMLLoader.load(getClass().getResource("master-password.fxml"));
+        Application.loadedStage.getScene().setRoot(pane);
+        Application.loadedStage.sizeToScene();
+        Application.loadedStage.centerOnScreen();
+    }
+
+    @FXML
+    protected void getAboutScene() throws IOException {
+        Parent pane = FXMLLoader.load(getClass().getResource("about.fxml"));
+        Application.loadedStage.getScene().setRoot(pane);
+        Application.loadedStage.sizeToScene();
+        Application.loadedStage.centerOnScreen();
+    }
+
+    @FXML
+    protected void getLoginScene() throws IOException {
+        Parent pane = FXMLLoader.load(getClass().getResource("login.fxml"));
+        Application.loadedStage.getScene().setRoot(pane);
+        Application.loadedStage.sizeToScene();
+        Application.loadedStage.centerOnScreen();
+    }
+
+
+
+
+
 }
